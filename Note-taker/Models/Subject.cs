0f61c9080e39b17d29baa9c0b49157257016569c
Danil Linkov <[danil.linkov@gmail.com]>
2020-09-12 18:07:51 +1200

    first commit

---
+++ Note-taker/Models/Subject.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Note_taker.Models
+{
+    public class Subject
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public string Title { get; set; }
+        [Required]
+        public string Description { get; set; }
+        [Required]
+        public ICollection<Note> Notes { get; set; }
+
+
+    }
+}
